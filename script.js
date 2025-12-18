52ad2418dbab33b40f21301009e3fedf37353871
CalebDuarte <caleb.jw.0309@gmail.com>
2025-12-17 17:22:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//generate a random number between 1-100
+
+//global variable 
+let randomNumber = Math.floor(Math.random() *100)+1;
+//console.log(randomNumber);
+let attempts = 0;
+
+//function to check useres guess
+function checkGuess(){
+    const guessInput = document.getElementById('guess');
+    const feedback = document.getElementById('feedback');
+    const userGuess = parseInt(guessInput.value);
+    //console.log(userGuess)
+
+    if(!userGuess || userGuess < 1 || userGuess > 100){
+        feedback.textContent = " Please enter a valid number between 1 and 100";
+        feedback.style.color='red';
+        return;
+    }
+
+    attempts++
+    if(userGuess === randomNumber){
+        feedback.textContent=`Congratulations ! You guessed the number in ${attempts} attempts`;
+        feedback.style.color='green';
+    }
+    else if(userGuess > randomNumber){
+        feedback.textContent="Too High ! Try again!";
+        feedback.style.color='red';
+    }
+    else{
+        feedback.textContent="Too Low ! Try again!";
+        feedback.style.color='red';
+    }
+    document.getElementById('guess').value = "";
+}
+
+//function resetgame()
+function resetGame(){
+    randomNumber = Math.floor(Math.random() *100) +1;
+    attempts = 0;
+    document.getElementById('guess').value = "";
+    document.getElementById('feedback').textContent = "";
+}
+
